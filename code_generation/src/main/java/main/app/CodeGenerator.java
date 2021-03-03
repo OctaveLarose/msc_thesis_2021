d1251bdcb3df839a706625fec0445bfdaa1b2e85
Octave Larose <octave.larose@hotmail.fr>
2021-03-03 15:50:12 +0000

    Switched to Gradle

--- code_generation/src/main/java/main/app/CodeGenerator.java
+++ code_generation/src/main/java/main/app/CodeGenerator.java
@@ -1,24 +1,24 @@
 package main.app;
-//import javapoet.*;
+import com.squareup.javapoet.*;
+import javax.lang.model.element.Modifier;
 
 public class CodeGenerator {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-//        MethodSpec main = MethodSpec.methodBuilder("main")
-//                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-//                .returns(void.class)
-//                .addParameter(String[].class, "args")
-//                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
-//                .build();
-//
-//        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
-//                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-//                .addMethod(main)
-//                .build();
-//
-//        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
-//                .build();
-//
-//        javaFile.writeTo(System.out);
+    public static void main(String[] args) throws java.io.IOException {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+                .build();
+
+        javaFile.writeTo(System.out);
     }
 }
