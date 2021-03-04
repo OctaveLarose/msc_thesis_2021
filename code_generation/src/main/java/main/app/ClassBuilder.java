73d9b93b4caa3555b01ac11427b29bad5887c898
Octave Larose <octave.larose@hotmail.fr>
2021-03-04 12:14:09 +0000

    Class linking PoC

--- code_generation/src/main/java/main/app/ClassBuilder.java
+++ code_generation/src/main/java/main/app/ClassBuilder.java
@@ -1,9 +1,11 @@
 package main.app;
 
+import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
 import javax.lang.model.element.Modifier;
+import java.util.List;
 import java.util.Random;
 
 public class ClassBuilder {
@@ -14,7 +16,7 @@ public class ClassBuilder {
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
 
         for (int i = 0; i < methodsNbr; i++)
-            outputClass.addMethod(this.generateMethod(generateRandomName(i + 5)));
+            outputClass.addMethod(this.generateBasicMethod(generateRandomName(i + 5)));
     }
 
     public TypeSpec build() {
@@ -32,12 +34,29 @@ public class ClassBuilder {
         return buffer.toString();
     }
 
-    private MethodSpec generateMethod(String name) {
+    public void addLinkedMethod(TypeSpec classSpecToLink) {
+        outputClass.addMethod(this.generateLinkedMethod(generateRandomName(10), classSpecToLink));
+    }
+
+    private MethodSpec generateBasicMethod(String name) {
         MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(name)
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                 .returns(void.class)
                 .addParameter(String[].class, "args")
                 .addStatement("$T.out.println($S)", System.class, "Hello world!");
+
+        return methodBuilder.build();
+    }
+
+    private MethodSpec generateLinkedMethod(String name, TypeSpec classSpecToLink) {
+        List<MethodSpec> methodsSpecs = classSpecToLink.methodSpecs;
+
+        // TODO: check how imports will be handled in that case. If the input class isn't imported, it won't compile
+        MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(name)
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(ClassName.bestGuess(classSpecToLink.name), "inputClass")
+                .addStatement("inputClass." + methodsSpecs.get(2).name + "()");
 
         return methodBuilder.build();
     }
--- code_generation/src/main/java/main/app/CodeGenerator.java
+++ code_generation/src/main/java/main/app/CodeGenerator.java
@@ -4,11 +4,16 @@ import javax.lang.model.element.Modifier;
 
 public class CodeGenerator {
     public static void main(String[] args) {
-        ClassBuilder classBuilder = new ClassBuilder("testClass", 20);
-        TypeSpec testClass = classBuilder.build();
+        ClassBuilder testClassBuilder = new ClassBuilder("TestClass", 5);
+
+        ClassBuilder helperClassBuilder = new ClassBuilder("HelperClass", 5);
+        TypeSpec HelperClass = helperClassBuilder.build();
+
+        testClassBuilder.addLinkedMethod(HelperClass);
+        TypeSpec testClass = testClassBuilder.build();
 
         ClassExporter classExporter = new ClassExporter("com.test.testClass", testClass);
         classExporter.exportToStdout();
-        classExporter.exportToFile();
+//        classExporter.exportToFile();
     }
 }
