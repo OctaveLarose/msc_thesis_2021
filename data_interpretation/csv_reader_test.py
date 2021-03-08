d321b3f8b0ecf8935fbf632e100b87fd2411b58d
Octave Larose <octave.larose@hotmail.fr>
2021-03-08 10:11:50 +0000

    Moving graph generation code around for cleanliness

--- data_interpretation/csv_reader_test.py
+++ data_interpretation/csv_reader_test.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+
+import data_interpretation.graph_generators.classes_per_project as classes_per_project
+import data_interpretation.graph_generators.methods_per_class as methods_per_class
 
 
 def generate_mega_class_csv():
@@ -23,100 +23,12 @@ def generate_mega_class_csv():
     df.to_csv("mega_class.csv", sep=',', encoding='utf-8')
 
 
-def get_classes_per_project_plot(df):
-    # df = pd.read_csv("/home/octavel/bordel/ck_data/class/class_jwtk_jjwt.csv")
-
-    data = zip(df["project_name"].unique(), [0] * len(df["project_name"].unique()))
-
-    classes_df = pd.DataFrame(data=data, columns=['projectName', 'classesNbr'])
-    for idx, project_name in enumerate(classes_df["projectName"]):
-        classes_df.at[idx, "classesNbr"] = len(df[df["project_name"] == project_name])
-
-    CLASS_PER_PROJECT_CAP = 3000
-
-    ### Seaborn version, has a nice theme
-    # classes_df = classes_df.loc[classes_df["classesNbr"] <= CLASS_PER_PROJECT_CAP]
-    # sns.set_theme(style="darkgrid")
-    # plot = sns.displot(data=classes_df, x="classesNbr")
-    # plot.set(xlabel='Number of classes', ylabel='Number of projects')
-    # plt.margins(x=0)
-    # plt.tight_layout()
-    # plt.show()
-    # plt.savefig("classes_per_project.png")
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={"hspace": 5})
-
-    # plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=None)
-
-    # sub_df = df.loc[df["totalMethodsQty"] <= METHOD_GRAPH_CAP]
-    # Config for both axes
-    ax1.set_ylabel('Number of projects')
-    for ax in [ax1, ax2]:
-        ax.set_xlabel('Number of classes')
-        ax.margins(x=0)
-
-    # Ax 1 config
-    bins = np.arange(-0.5, CLASS_PER_PROJECT_CAP + 1, 100)
-    ax1.hist(classes_df["classesNbr"], bins=bins)
-    ax1.set_xlim(right=CLASS_PER_PROJECT_CAP)
-
-    # Ax 2 config
-    max_len = classes_df["classesNbr"].max()
-    bins = np.arange(0, max_len + 1, 100)
-    ax2.hist(classes_df["classesNbr"], bins=bins)
-    ax2.set_yscale('log')
-    ax2.set_xlim([0, max_len])
-    # ax2.set_ylim(top=100)
-    plt.show()
-
-
-def get_methods_per_class_plot(df):
-    # df = pd.read_csv("/home/octavel/bordel/ck_data/class/class_jwtk_jjwt.csv")
-
-    METHOD_GRAPH_CAP = 30
-    # print(f"Classes with nbr methods <= {METHOD_GRAPH_CAP}: {len(df.loc[df['totalMethodsQty'] <= METHOD_GRAPH_CAP])}")
-    # print(f"Classes with nbr methods > {METHOD_GRAPH_CAP}: {len(df.loc[df['totalMethodsQty'] > METHOD_GRAPH_CAP])}")
-
-    # plot = sns.displot(data=sub_df, x="totalMethodsQty", bins=bins)
-    # plot.set(xlabel='Number of methods', ylabel='Number of classes')
-    # plt.xlim(-1, 30)
-    # plt.tight_layout()
-    #
-    # plot = sns.displot(data=df, x="totalMethodsQty")
-    # plot.set(xlabel='Number of methods', ylabel='Number of classes')
-    fig, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={"hspace": 5})
-
-    # plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=None)
-
-    # sub_df = df.loc[df["totalMethodsQty"] <= METHOD_GRAPH_CAP]
-    # Config for both axes
-    ax1.set_ylabel('Number of classes')
-    for ax in [ax1, ax2]:
-        ax.set_xlabel('Number of methods')
-        ax.margins(x=0)
-
-    # Ax 1 config
-    bins = np.arange(-0.5, METHOD_GRAPH_CAP + 1, 1)
-    ax1.hist(df["totalMethodsQty"], bins=bins)
-    ax1.set_xlim(right=METHOD_GRAPH_CAP)
-
-    # Ax 2 config
-    max_len = df["totalMethodsQty"].max()
-    bins = np.arange(0, max_len + 1, 20)
-    ax2.hist(df["totalMethodsQty"], bins=bins)
-    ax2.set_yscale('log')
-    ax2.set_xlim([0, max_len])
-    # ax2.set_ylim(top=100)
-    plt.show()
-    # plt.savefig("methods_per_class.png")
-
-
 def main():
     # generate_mega_class_csv()
 
     df = pd.read_csv("mega_class.csv")
-    # get_methods_per_class_plot(df)
-    get_classes_per_project_plot(df)
+    methods_per_class.generate_graph(df)
+    # classes_per_project.generate_graph()
 
 
 if __name__ == "__main__":
